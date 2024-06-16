53d48b36e17947b9fa725374c7f276320509ce11
skaufmann <simon.kaufmann@adesso.ch>
2024-06-16 15:00:02 +0200

    Add GET by id and POST for profiles

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -3,43 +3,41 @@ const router = express.Router();
 const Profile = require('../models/profile');
 const faker = require('faker');
 
+// temporary storage to fake a database
+let temporaryStoredProfiles = Array.from({length: 10}, () => ({
+  name: faker.name.firstName() + " " + faker.name.lastName(),
+  email: faker.internet.email(),
+  location: faker.address.city(),
+  skills: [mockSkills()],
+  projects: [mockProjects()]
+}));
+
 // Get all profiles
 router.get('/', async (req, res) => {
-  // Mock data
-  const mockProfiles = Array.from({ length: 10 }, () => ({
-    name: faker.name.firstName() + " " + faker.name.lastName(),
-    email: faker.internet.email(),
-    location: faker.address.city(),
-    skills: [mockSkills()],
-    projects: [mockProjects()]
-  }));
-
-  res.json(mockProfiles);
+  res.json(temporaryStoredProfiles);
 });
 
 // Get a specific profile
 router.get('/:id', async (req, res) => {
-  // Mock data
-  const mockProfile = {
-    name: faker.name.firstName() + " " + faker.name.lastName(),
-    email: faker.internet.email(),
-    location: faker.address.city(),
-    skills: [mockSkills()],
-    projects: [mockProjects()]
-  };
-  res.json(mockProfile);
+  const profileId = req.params.id;
+  const profile = temporaryStoredProfiles.find(profile => profile.id === profileId);
+  if (profile) {
+    res.json(profile);
+  } else {
+    res.status(404).json({message: 'Profile not found'});
+  }
 });
 
 // Create a new profile
 router.post('/', async (req, res) => {
   const newProfile = new Profile(req.body);
-  const savedProfile = await newProfile.save();
-  res.json(savedProfile);
+  temporaryStoredProfiles.push(newProfile);
+  res.json(newProfile);
 });
 
 // Update a profile
 router.put('/:id', async (req, res) => {
-  const updatedProfile = await Profile.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const updatedProfile = await Profile.findByIdAndUpdate(req.params.id, req.body, {new: true});
   res.json(updatedProfile);
 });
 
@@ -53,14 +51,14 @@ module.exports = router;
 
 // helper functions
 function mockSkills() {
-  return [Array.from({ length: Math.floor(Math.random() * 10) }, () => ({
-      name: faker.lorem.word(),
-      experience: Math.floor(Math.random() * 5) + 1
+  return [Array.from({length: Math.floor(Math.random() * 10)}, () => ({
+    name: faker.lorem.word(),
+    experience: Math.floor(Math.random() * 5) + 1
   }))]
 }
 
 function mockProjects() {
-  return [Array.from({ length: Math.floor(Math.random() * 5) }, () => ({
+  return [Array.from({length: Math.floor(Math.random() * 5)}, () => ({
     name: faker.lorem.word(),
     description: faker.lorem.paragraph()
   }))]
