cf33bbd4bd4eca238ad91b4947174aa123262cc3
René Nienaber <rene.nienaber@adesso.ch>
2024-06-16 14:40:55 +0200

    mock skills and projects for /profiles getters

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res) => {
     name: faker.name.firstName() + " " + faker.name.lastName(),
     email: faker.internet.email(),
     location: faker.address.city(),
-    skills: [],
-    projects: []
+    skills: [mockSkills()],
+    projects: [mockProjects()]
   }));
 
   res.json(mockProfiles);
@@ -24,8 +24,8 @@ router.get('/:id', async (req, res) => {
     name: faker.name.firstName() + " " + faker.name.lastName(),
     email: faker.internet.email(),
     location: faker.address.city(),
-    skills: [],
-    projects: []
+    skills: [mockSkills()],
+    projects: [mockProjects()]
   };
   res.json(mockProfile);
 });
@@ -50,3 +50,18 @@ router.delete('/:id', async (req, res) => {
 });
 
 module.exports = router;
+
+// helper functions
+function mockSkills() {
+  return [Array.from({ length: Math.floor(Math.random() * 10) }, () => ({
+      name: faker.lorem.word(),
+      experience: Math.floor(Math.random() * 5) + 1
+  }))]
+}
+
+function mockProjects() {
+  return [Array.from({ length: Math.floor(Math.random() * 5) }, () => ({
+    name: faker.lorem.word(),
+    description: faker.lorem.paragraph()
+  }))]
+}
