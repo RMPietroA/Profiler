32378f9e834e25d1837e43a69822cb25724ff491
skaufmann <simon.kaufmann@adesso.ch>
2024-06-16 11:14:17 +0200

    Restart from scratch

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import auth from './auth_reducer';
-import user from './user_reducer';
-
-export default {
-  user,
-  auth
-};
--- server/src/util/logger.js
+++
@@ -1,6 +0,0 @@
-import getLogger from 'loglevel-colored-level-prefix';
-
-const options = {prefix: 'mern-server', level: 'trace'}
-const logger = getLogger(options)
-
-export default logger
