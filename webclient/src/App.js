50dabbec8221ae8f3559258d6d30abda1e70a82f
Duric Ranko, INI-DOS-BLG (EXT) <Ranko.Duric@swisscom.com>
2024-06-16 14:15:20 +0200

    DEV: add menu item to list all profiles and add profile

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AddProfile from './AddProfile';
+import ListProfiles from './ListProfiles';
 import './App.css';
-
+import './Menu.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link className="navbar-item" to="/">List all profiles</Link>
+            </li>
+            <li>
+              <Link className="navbar-item" to="/add-profile">Add profile</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ListProfiles />} />
+          <Route path="/add-profile" element={<AddProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ webclient/src/ListProfiles.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function ListProfiles() {
+
+  return (
+    <div>
+      <h1>List of Profiles</h1>
+    </div>
+  );
+}
+
+export default ListProfiles;
