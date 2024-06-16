95bc284dd76b2228b328718da931ff9443b5d5f6
René Nienaber <rene.nienaber@adesso.ch>
2024-06-16 11:59:19 +0200

    fix backend port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,3 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
